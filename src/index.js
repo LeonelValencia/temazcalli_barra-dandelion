e7b3af5d7ec8017885ea175cd5d7de08116551db
Leonel Valencia <valenciaestudillo@gmail.com>
2023-03-13 22:33:50 -0600

    eslint y prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
