5aaed63a3084e46a31f821fbd738f7eec7fb6844
Leonel Valencia <valenciaestudillo@gmail.com>
2023-03-25 14:25:48 -0600

    [add] react-helmet 2

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -5,11 +5,6 @@ import Helmet from 'react-helmet';
 import '../styles/components/Checkout.css'
 
 const Checkout = () => {
-    <>
-    <Helmet>
-        <title>Lista de productos - Merch</title>
-    </Helmet>
-    </>
     const { state, removeFromCart } = useContext(AppContex);
     const { cart } = state;
 
@@ -24,30 +19,35 @@ const Checkout = () => {
     }
 
     return (  
-        <div className="Checkout">
-            <div className="Checkout-content">
-                {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3> }
-                {cart.map((item, i) => (
-                    <div className="Checkout-item" key={i+item.title}>
-                        <div className="Checkout-element">
-                            <h4>{item.title}</h4>
-                            <span>${item.price}</span>
+        <>
+            <Helmet>
+                <title>Lista de pedidos - Merch</title>
+            </Helmet>
+            <div className="Checkout">
+                <div className="Checkout-content">
+                    {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3> }
+                    {cart.map((item, i) => (
+                        <div className="Checkout-item" key={i+item.title}>
+                            <div className="Checkout-element">
+                                <h4>{item.title}</h4>
+                                <span>${item.price}</span>
+                            </div>
+                            <button type='button' onClick={handleRemove(item, i)}>
+                                <li className='fas fa-trash-alt'/>
+                            </button>
                         </div>
-                        <button type='button' onClick={handleRemove(item, i)}>
-                            <li className='fas fa-trash-alt'/>
-                        </button>
-                    </div>
-                ))}
-            </div>
-            {cart.length > 0 && (
-                <div className="Checkout-sidebar">
-                    <h3>{`Precio Total: $${handleSumTotal()}`}</h3>
-                    <Link to='/checkout/information'>
-                        <button type='button'>Continuar pedido</button>
-                    </Link>
+                    ))}
                 </div>
-            )}
-        </div>
+                {cart.length > 0 && (
+                    <div className="Checkout-sidebar">
+                        <h3>{`Precio Total: $${handleSumTotal()}`}</h3>
+                        <Link to='/checkout/information'>
+                            <button type='button'>Continuar pedido</button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
  
